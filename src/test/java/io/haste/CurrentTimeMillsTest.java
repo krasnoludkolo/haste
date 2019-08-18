83c40ab2a35f178564f5ea189d01e8d93c37d1bf
janek@projmen.pl <janek@projmen.pl>
2019-08-18 14:52:29 +0200

    Add more tests

--- src/test/java/io/haste/CurrentTimeMillsTest.java
+++ src/test/java/io/haste/CurrentTimeMillsTest.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.Test;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 final class CurrentTimeMillsTest {
 
     @Test
-    void shouldReturnCorrectMillisTime() {
+    void shouldReturnCorrectMillisTimeForCreatedMovableTimeSource() {
         Instant instant = Instant.ofEpochMilli(0);
         ZoneId zoneId = ZoneId.systemDefault();
         Clock clock = Clock.fixed(instant, zoneId);
@@ -21,6 +22,46 @@ final class CurrentTimeMillsTest {
 
         assertEquals(0, currentTime);
     }
+
+    @Test
+    void shouldReturnCorrectMillisTimeForMovableTimeSourceWithMovedTime() {
+        Instant instant = Instant.ofEpochMilli(0);
+        ZoneId zoneId = ZoneId.systemDefault();
+        Clock clock = Clock.fixed(instant, zoneId);
+        MovableTimeSource timeSource = Haste.TimeSource.withFixedClock(clock);
+
+        timeSource.advanceTimeBy(10, TimeUnit.MILLISECONDS);
+
+        long currentTime = timeSource.currentTimeMillis();
+        assertEquals(10, currentTime);
+    }
+
+
+    @Test
+    void shouldReturnCorrectMillisTimeForCreatedBlockingScheduledExecutionService() {
+        Instant instant = Instant.ofEpochMilli(0);
+        ZoneId zoneId = ZoneId.systemDefault();
+        Clock clock = Clock.fixed(instant, zoneId);
+        BlockingScheduledExecutionService service = Haste.ScheduledExecutionService.withFixedClock(clock);
+
+        long currentTime = service.currentTimeMillis();
+        assertEquals(0, currentTime);
+    }
+
+    @Test
+    void shouldReturnCorrectMillisTimeForBlockingScheduledExecutionServiceWithMovedTime() {
+        Instant instant = Instant.ofEpochMilli(0);
+        ZoneId zoneId = ZoneId.systemDefault();
+        Clock clock = Clock.fixed(instant, zoneId);
+        BlockingScheduledExecutionService service = Haste.ScheduledExecutionService.withFixedClock(clock);
+
+        service.advanceTimeBy(10, TimeUnit.MILLISECONDS);
+
+        long currentTime = service.currentTimeMillis();
+        assertEquals(10, currentTime);
+    }
+
+
 
 
 }
